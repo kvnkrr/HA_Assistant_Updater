fc76782ba3696c1787bb24e78282d1015b0ec674
selflearn <121253179+kvnkrr@users.noreply.github.com>
2023-01-03 01:52:25 +0000

    Update Update.sh

--- Update.sh
+++ Update.sh
@@ -2,4 +2,4 @@
 docker stop ha_automationhelper
 docker rm ha_automationhelper
 docker rmi kvnkrruk/ha_automationhelper
-sudo docker run -t -d -p 9002:9005 kvnkrruk/ha_automationhelper 
+sudo docker run -t -d -p 9002:9005 --name my-helper-app kvnkrruk/ha_automationhelper 
